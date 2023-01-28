ad0cd81159ab0c2929441b630b6e95df4fe0277a
HomeProgrammer81 <master@home-programming.com>
2023-01-28 09:18:56 +0900

    円グラフ 強調

--- OxyPlotProject/OxyPlotPlotModel/PieSeriesPlotModelFactory.cs
+++ OxyPlotProject/OxyPlotPlotModel/PieSeriesPlotModelFactory.cs
@@ -11,13 +11,22 @@ namespace OxyPlotProject.OxyPlotPlotModel
     /// </summary>
     internal class PieSeriesPlotModelFactory : AbstPlotModelFactory
     {
-        private List<Tuple<string, double>> pieDataList = new List<Tuple<string, double>>()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Item1 : ラベル名
+        /// Item2 : 商品売り上げ値
+        /// Item3 : 切り抜きフラグ
+        /// Item4 : 背景色
+        /// </remarks>
+        private List<Tuple<string, double,bool,OxyColor>> PieSliceList = new List<Tuple<string, double,bool, OxyColor>>()
         {
-            new Tuple<string, double>( "商品A", 25394),
-            new Tuple<string, double>( "商品B", 20343),
-            new Tuple<string, double>( "商品C", 15400),
-            new Tuple<string, double>( "商品D", 10200),
-            new Tuple<string, double>( "その他", 9200),
+            new Tuple<string, double,bool,OxyColor>( "商品A", 25394, true, OxyColors.Red),
+            new Tuple<string, double,bool,OxyColor>( "商品B", 20343, false, OxyColors.DarkGray),
+            new Tuple<string, double,bool,OxyColor>( "商品C", 15400, false, OxyColors.DarkGray),
+            new Tuple<string, double,bool,OxyColor>( "商品D", 10200, false, OxyColors.DarkGray),
+            new Tuple<string, double,bool,OxyColor>( "その他", 9200, false, OxyColors.DarkGray),
         };
 
         public override PlotModel Create()
@@ -41,11 +50,14 @@ namespace OxyPlotProject.OxyPlotPlotModel
             pieSeries.TickDistance = 4;                     // 円の端と、割合の線の距離
 
             pieSeries.StartAngle = -90;                     // データの開始角度
-
+            pieSeries.ExplodedDistance = 0.1;
             // データ設定
-            foreach( Tuple<string, double> tuple in pieDataList )
+            foreach( Tuple<string, double, bool, OxyColor> tuple in PieSliceList)
             {
                 PieSlice pieSlice = new PieSlice(tuple.Item1, tuple.Item2);
+                pieSlice.IsExploded = tuple.Item3;
+                pieSlice.Fill = tuple.Item4;
+
                 pieSeries.Slices.Add(pieSlice);
             }
 
